2402dc1fcadd964f627bb93212db32df6cb427e0
flipsflops <victoribasco18@gmail.com>
2023-03-20 17:04:22 -0700

    reverted components

--- src/components/codeChallenge/Cars.jsx
+++ src/components/codeChallenge/Cars.jsx
@@ -1,8 +1,4 @@
 /*
-Do not modify unless working on React Mapping Assessment.
+Do not modify or add to this page unless working on React Practice Assessment.
 */
-function Cars() {
-	return <div className="container">Cars Here</div>;
-}
-
-export default Cars;
+console.log("DELETE THIS LINE");
